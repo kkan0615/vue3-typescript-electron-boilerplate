604b48dcb6fe957b0b56e162116b510c18799fa7
kkan0615 <kkan0615@gmail.com>
2022-07-20 22:20:34 -0400

    :zap: Feat: add checking network for updates

--- packages/main/src/main.ts
+++ packages/main/src/main.ts
@@ -1,7 +1,7 @@
-import { app, Notification, dialog, ipcMain, BrowserWindow } from 'electron'
+import { app } from 'electron'
 import { createAppWindow } from './windows/app'
 import { autoUpdater } from 'electron-updater'
-import isDev from 'electron-is-dev'
+import './utils/auto-updete'
 
 const isSingleInstance = app.requestSingleInstanceLock()
 
@@ -35,52 +35,3 @@ app
     await autoUpdater.checkForUpdates()
   })
   .catch((e) => console.error('Failed to create window:', e))
-
-
-autoUpdater.on('error', async (error, message) => {
-  if (!isDev) {
-    await dialog.showMessageBox(null as any, {
-      type: 'error',
-      title: 'Error: auto-updater',
-      message: 'Error: auto-updater',
-      detail: message,
-    })
-  }
-})
-
-autoUpdater.on('update-not-available', async () => {
-  BrowserWindow.getAllWindows().map(window => {
-    window.webContents.send('update-not-available')
-  })
-})
-
-/**
- * Check update when update is available when program is executed.
- */
-autoUpdater.on('update-available', (info) => {
-  // @TODO: Change to confirm
-  new Notification({
-    title: 'update is available',
-    body: `${info.version} is available`,
-  }).show()
-
-  BrowserWindow.getAllWindows().map(window => {
-    window.webContents.send('update-available')
-  })
-})
-
-/**
- * check update is ready
- * @return boolean - if update is ready return true, else, return false
- */
-ipcMain.handle('check-update',async () => {
-  await autoUpdater.checkForUpdates()
-})
-
-/**
- * Update program
- */
-ipcMain.on('update-program',async () => {
-  await autoUpdater.quitAndInstall()
-})
-
